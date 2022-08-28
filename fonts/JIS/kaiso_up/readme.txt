fad3488421570c8a3472b0a0ff2418761604304c
Kizeko <paul.j.petibon@gmail.com>
2022-08-28 09:53:56 -0400

    removed fonts, made main image python script

---
+++ fonts/JIS/kaiso_up/readme.txt
@@ -0,0 +1,46 @@
+この度は、『廻想体 ネクストユーピー（B）』を
+ダウンロードいただきありがとうございます。
+印刷物、映像関連の商用利用など、ご自由にご活用ください。
+●概要
+Format: OpenType / TrueType  
+Licence: フリーフォント（商用可）
+収録字数：約4,000字
+収録詳細：ひらかな・カタカナ・半角英数字・記号
+　　　　　JIS第一水準（2965字）JIS第二水準（890字）他
+●ライセンス詳細
+　https://moji-waku.com/mj_work_licence/
+●ご注意
+※無断でのデータのアップロード・データの改変、改造・ニ次配布は、お控えいただくようご協力のほどお願い致します。
+※国内・国外のフリーフォント投稿サイト及フォント投稿サイトへのフォントデータ投稿は禁止しています。
+※廻想体ネクストユーピー（B）を使用し、その結果として発生した機械およびソフトウエアの故障、
+　又は誤動作・誤出力に関して、「もじワク研究」は責任を負わないものとします。
+ご不明な点がございましたら、下記のアドレスまでご連絡ください。
+
+
+We are pleased to announce that "Kaiso-tai Next　UP B"
+Thank you for downloading this file.
+Please feel free to use it for commercial use, such as for printed materials and images.
+
+[Overview]
+Format: OpenType / TrueType  
+Licence: Free(commercial use OK)
+Characters：4,000
+CJK(JIS1:2965) (JIS2:890)
+Hirakana, Katakana, Euro, Symbol
+[Licence]
+https://moji-waku.com/mj_work_licence/
+
+(1)Please refrain from uploading, altering, modifying or distributing the data without permission.
+
+(2)It is prohibited to submit font data to free font submission sites and font submission sites in Japan and abroad.
+
+(3)Mechanical and software failures resulting from the use of Kaiso-tai Next UP B, and You will not be held responsible for any malfunctions or errors in operation or output.
+
+If you have any questions, please contact us at the address below.
+
+
+もじワク研究
+ひろきちゃん
+Moji-Waku Kenkyu／HIROKI-CHAN
+WEB: https://moji-waku.com
+E-Mail: info@moji-waku.com
---
+++ generate_images.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Pre processing and generation of images
+
+# In[13]:
+
+
+from PIL import Image, ImageFont, ImageDraw
+import os
+import numpy as np
+import cv2
+import pandas as pd
+from fontTools.ttLib import TTFont
+from tqdm.auto import tqdm
+
+
+# In[14]:
+
+
+width, height = 60, 60
+def generate_image(character, font_path, image_path):
+    background_color = 'black'
+    text_color = (255)
+    color_mode = 'L'
+    font_size=55
+
+
+    font = ImageFont.truetype(font=font_path, size=font_size)
+    image = Image.new(mode=color_mode, size=(width, height), color=background_color)
+    image_draw = ImageDraw.Draw(image)
+
+    bbox = image_draw.textbbox((0, 0), text=chr(character), font=font)
+    xy = ((width - (bbox[2] + bbox[0])) / 2, (height - (bbox[3] + bbox[1])) / 2)
+
+    image_draw.text(xy=xy, text=chr(character), fill=text_color, font=font)
+
+    for i in range(width):
+        for j in range(height):
+            if image.getpixel((i, j)) != 0:
+                if not os.path.exists("../Kakiburi-Images/U+{}".format(hex(character)[2:])):
+                    os.mkdir(os.path.dirname(image_path))
+                image.save(image_path)
+                return True
+    
+    return False
+
+
+# In[15]:
+
+
+unicodes = [0x3000,
+            0x303f,
+            0x3041,
+            0x309f,
+            0x30a0,
+            0x30ff,
+            0xff00,
+            0xffef,
+            0x4e00,
+            0x9faf]
+
+# store in a variable the sum of the total unicodes from the list of 2 paired ranges defined above
+total_unicodes = 0
+for i in range(len(unicodes), 0, -2):
+    total_unicodes += unicodes[i - 1] - unicodes[i - 2]
+
+
+# In[16]:
+
+
+infos = pd.read_csv("./fonts.csv", index_col=0)
+infos.head()
+
+
+# In[17]:
+
+
+def char_in_font(unicode_char, font):
+    for cmap in font['cmap'].tables:
+        if cmap.isUnicode():
+            if ord(unicode_char) in cmap.cmap:
+                return True
+    return False
+
+
+# In[18]:
+
+
+dataset_x = []
+dataset_y = []
+
+for index, info in infos.iterrows():
+    i = 0
+    while i < len(unicodes) - 1:
+        start = unicodes[i]
+        end = unicodes[i + 1]
+        print("part (U+{})-(U+{}) of the font {} :".format(hex(start)[2:], hex(end)[2:], info['name']))
+        for j in tqdm(range(start, end)):
+            font = TTFont(info['path'])
+            if not char_in_font(chr(j), font):
+                continue
+
+            img_path = "../Kakiburi-Images/U+{}/{}.png".format(hex(j)[2:], index)
+
+            valid = generate_image(j, info['path'], img_path)
+            if not valid:
+                continue
+
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            dataset_x.append(img)
+            dataset_y.append(j)
+        i += 2
+
+
+np.save("./dataset_x.npy", np.array(dataset_x).reshape(-1, width, height, 1))
+np.save("./dataset_y.npy", np.array(dataset_y).reshape(-1, 1))
+                        
+
+
+# In[ ]:
+
+
+
+
